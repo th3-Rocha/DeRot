4f556692b18d379135b06549b9dfa5a735348aff
JCassio <josecassio2013@gmail.com>
2025-09-28 14:14:41 -0300

    setup: add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { crx } from "@crxjs/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import manifest from "./manifest.json" assert { type: "json" };
 import type { ManifestV3Export } from "@crxjs/vite-plugin";
 
 const typedManifest = manifest as ManifestV3Export;
 
 export default defineConfig({
-  plugins: [react(), crx({ manifest: typedManifest })],
+  plugins: [react(), crx({ manifest: typedManifest }), tailwindcss()],
 });
